b91d04134c6b4f8ca0ae07f0f4bf3d7ba2149e9c
Joshua Van Baalen <joshuavanbaalen@gmail.com>
2018-05-02 14:19:53 -0600

    Implemented quick sort

--- main.cpp
+++ main.cpp
@@ -9,12 +9,32 @@ using std::cout;
 using std::endl;
 
 int main() {
-	vector<int> vec1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	vector<int> vec1 = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-
+	cout << "If binSearch returns -1, element is not in vector:" << endl;
 	cout << "vector int binsearch 4: " << binSearch(vec1, 4, 0) << endl;
 	cout << "vector int binsearch 8: " << binSearch(vec1, 8, 0) << endl;
-	cout << "vector int binsearch 10: " << binSearch(vec1, 10, 0);
+	cout << "vector int binsearch 10: " << binSearch(vec1, 10, 0) << endl << endl;
+	//
+
+	vector<int> vec2 = { 11, 6, 77, 8, 0, 22, 35, 100 };
+	vector<int> vec3;
+
+	vector<char> vec4 = { 'c', 'a', 'z', '0', '`', 'b' };
+	vector<char> vec5;
+
+	vec3 = QuickSort(vec2, vec3);
+	vec5 = QuickSort(vec4, vec5);
+
+	cout << "quick sort vec2: ";
+	for (int x = 0; x < vec3.size(); x++) {
+		cout << vec3[x] << " ";
+	}
+	cout << endl;
+	cout << "quick sort vec4: ";
+	for (int x = 0; x < vec5.size(); x++) {
+		cout << vec5[x] << " ";
+	}
 
 
 	int test;
--- project2.cpp
+++ project2.cpp
@@ -27,3 +27,73 @@ int binSearch(vector<T> t1, T2 t2, int loop) {
 	}
 	return binSearch(tempVec, t2, loop);
 }
+
+template <class T>
+
+vector<T> QuickSort(vector<T>& vec1, vector<T>& vec2) {
+	vector <T> vecTemp = vec1;
+	int right = vecTemp.size();
+	int left = 0;
+	int mid = right / 2;
+
+	for (int x = 0; x < 10; x++) {
+		vecTemp = qSort(vecTemp, left, right);
+		vecTemp = qSort(vecTemp, left, mid);
+		vecTemp = qSort(vecTemp, mid, right);
+	}
+
+
+	vec2 = vecTemp;
+
+	return vec2;
+}
+
+template <class T>
+
+vector<T> qSort(vector <T> vec, int L, int R) {
+	int pivot = (R + L) / 2;
+	vector<T> vecLow;
+	vector<T> vecHigh;
+	vector<T> vecCopy = vec;
+	vec = {};
+
+	if (L > 0) {
+		for (int x = 0; x < L; x++) {
+			vec.push_back(vecCopy[x]);
+		}
+	}
+
+	for (int x = L; x < pivot; x++) {
+		if (vecCopy[x] < vecCopy[pivot]) {
+			vecLow.push_back(vecCopy[x]);
+		}
+		else if (vecCopy[x] > vecCopy[pivot]) {
+			vecHigh.push_back(vecCopy[x]);
+		}
+	}
+
+	for (int x = pivot; x < R; x++) {
+		if (vecCopy[x] < vecCopy[pivot]) {
+			vecLow.push_back(vecCopy[x]);
+		}
+		else if (vecCopy[x] > vecCopy[pivot]) {
+			vecHigh.push_back(vecCopy[x]);
+		}
+	}
+
+	for (int x = 0; x < vecLow.size(); x++) {
+		vec.push_back(vecLow[x]);
+	}
+	vec.push_back(vecCopy[pivot]);
+	for (int x = 0; x < vecHigh.size(); x++) {
+		vec.push_back(vecHigh[x]);
+	}
+
+	if (R < vecCopy.size()) {
+		for (int x = R; x < vecCopy.size(); x++) {
+			vec.push_back(vecCopy[x]);
+		}
+	}
+
+	return vec;
+}
